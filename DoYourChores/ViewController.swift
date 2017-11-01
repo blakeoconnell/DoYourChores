5c133a7fb8d53d20e15005695d6a4c76b0282713
Blake O'Connell <blakeoconnell@Blakes-MacBook-Air.local>
2017-10-31 17:53:08 -0700

    Initial Commit

---
+++ DoYourChores/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoYourChores
+//
+//  Created by Blake O'Connell on 10/31/17.
+//  Copyright © 2017 Blake O'Connell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
